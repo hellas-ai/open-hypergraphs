24126116fda248f2e34b92531b8cc7c02e18f00b
statusfailed <paul@statusfailed.com>
2025-01-30 12:05:12 +0000

    add test for associativity of composition

--- tests/open_hypergraph/test_open_hypergraph.rs
+++ tests/open_hypergraph/test_open_hypergraph.rs
@@ -35,7 +35,7 @@ proptest! {
 
     #[test]
     fn test_composition_wire_count(v in arb_composite_open_hypergraph(2)) {
-        let [f, g] = v.as_slice() else { panic!("arb_composite_open_hypergraph returned wrong number of open hypergraphs") };
+        let [f, g] = v.as_slice() else { panic!("arb_composite_open_hypergraph returned unexpected size result") };
         let h = (f.compose(g)).unwrap();
 
         assert_eq!(h.source(), f.source());
@@ -46,5 +46,14 @@ proptest! {
 
         // ... and *more* than or equal to the number of wires in f, g less those on the boundary.
         assert!(h.h.w.len() >= f.h.w.len() + g.h.w.len() - f.t.source());
+    }
+
+    #[test]
+    fn test_composition_associative(fgh in arb_composite_open_hypergraph(3)) {
+        let [f, g, h] = fgh.as_slice() else { panic!("arb_composite_open_hypergraph returned unexpected size result") };
+        let x = (f.compose(g)).unwrap().compose(h).unwrap();
+        let y = f.compose(&g.compose(h).unwrap()).unwrap();
+
+        assert_open_hypergraph_equality_invariants(&x, &y);
     }
 }
