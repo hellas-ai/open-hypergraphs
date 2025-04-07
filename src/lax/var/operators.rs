8fc9810c535d815824a655a5908d438ad04265b5
statusfailed <paul@statusfailed.com>
2025-04-07 13:32:04 +0100

    lax::var: replace `binop` with n-ary `operation`..
    .. and derive more operations using the `define_binary_op!` macro.

--- src/lax/var/operators.rs
+++ src/lax/var/operators.rs
@@ -1,38 +1,37 @@
+use crate::lax::hypergraph::Hyperedge;
+use crate::lax::open_hypergraph::OpenHypergraph;
+
 use super::var::*;
-use std::ops::{Add, BitAnd, BitOr, BitXor};
 
-// helper function to create binary ops
-fn binop<O: Clone, A: HasVar>(lhs: Var<O, A>, rhs: Var<O, A>, result_label: O, op: A) -> Var<O, A> {
-    // Get nodes for lhs and rhs vars
-    let lhs_node = lhs.new_target();
-    let rhs_node = rhs.new_target();
+use std::cell::RefCell;
+use std::ops::*;
+use std::rc::Rc;
 
-    // Get source/target types
-    let source_type = vec![lhs.label.clone(), rhs.label.clone()];
-    let target_type = vec![result_label.clone()];
+/// A general helper for constructing `n → 1` maps
+pub fn operation<O: Clone, A: HasVar>(
+    // TODO: generalise to something which borrows a mutable OpenHypergraph?
+    builder: &Rc<RefCell<OpenHypergraph<O, A>>>,
+    vars: &[Var<O, A>],
+    result_label: O,
+    op: A,
+) -> Var<O, A> {
+    let mut nodes = Vec::with_capacity(vars.len());
+    for v in vars {
+        nodes.push(v.new_target());
+    }
 
-    let target = {
-        let mut state = lhs.state.borrow_mut();
+    let v = Var::new(builder.clone(), result_label);
+    let v_source = v.new_source();
 
-        // Create a new Add operation
-        let (_, (s, t)) = state.new_operation(op, source_type, target_type);
+    let mut term = builder.borrow_mut();
+    let _ = term.new_edge(
+        op,
+        Hyperedge {
+            sources: nodes,
+            targets: vec![v_source],
+        },
+    );
 
-        // op is a binary operation by construction
-        assert_eq!(s.len(), 2);
-        assert_eq!(t.len(), 1);
-
-        // Connect the input vars to the new operation
-        state.unify(s[0], lhs_node);
-        state.unify(s[1], rhs_node);
-
-        t[0]
-    };
-
-    // Create a Var for the result, unify op target (t[0]) with its input, and return the Var.
-    // TODO: helper method to create a var from a node?
-    let v = Var::new(rhs.state.clone(), result_label);
-    let v_source = v.new_source(); // new_source calls borrow_mut, so must be done first.
-    v.state.borrow_mut().unify(target, v_source); // unify target from above with source of v.
     v
 }
 
@@ -47,7 +46,7 @@ impl<O: Clone, A: HasVar + HasAdd<O, A>> Add for Var<O, A> {
     fn add(self, rhs: Self) -> Self::Output {
         // only difference between impls
         let (result_label, op) = A::add(self.label.clone(), rhs.label.clone());
-        binop(self, rhs, result_label, op)
+        operation(&self.state.clone(), &[self, rhs], result_label, op)
     }
 }
 
@@ -62,7 +61,7 @@ impl<O: Clone, A: HasVar + HasBitXor<O, A>> BitXor for Var<O, A> {
     fn bitxor(self, rhs: Self) -> Self::Output {
         // only difference between impls
         let (result_label, op) = A::bitxor(self.label.clone(), rhs.label.clone());
-        binop(self, rhs, result_label, op)
+        operation(&self.state.clone(), &[self, rhs], result_label, op)
     }
 }
 
@@ -79,11 +78,22 @@ macro_rules! define_binary_op {
 
             fn $fn_name(self, rhs: Self) -> Self::Output {
                 let (result_label, op) = A::$fn_name(self.label.clone(), rhs.label.clone());
-                binop(self, rhs, result_label, op)
+                //binop(self, rhs, result_label, op)
+                operation(&self.state.clone(), &[self, rhs], result_label, op)
             }
         }
     };
 }
 
+//define_binary_op!(BitXor, bitand, HasBitXor); // hand-written
 define_binary_op!(BitAnd, bitand, HasBitAnd);
 define_binary_op!(BitOr, bitor, HasBitOr);
+define_binary_op!(Shl, shl, HasShl);
+define_binary_op!(Shr, shr, HasShr);
+//TODO: Not
+
+//define_binary_op!(Add, add, HasAdd); // hand-written
+define_binary_op!(Mul, mul, HasMul);
+define_binary_op!(Sub, sub, HasSub);
+define_binary_op!(Div, div, HasDiv);
+// TODO: neg
