bab8555f68af97bb57b264d147c8a0d47e99b3c9
mstn <mstn@posteo.org>
2026-02-17 18:04:36 +0100

    gate rewrite api behind feature flag

--- src/strict/open_hypergraph/mod.rs
+++ src/strict/open_hypergraph/mod.rs
@@ -1,8 +1,11 @@
 //! The primary datastructure for representing cospans of hypergraphs
 mod arrow;
+#[cfg(feature = "experimental")]
 mod rewrite;
 #[cfg(test)]
+#[cfg(feature = "experimental")]
 mod rewrite_tests;
 
 pub use arrow::*;
+#[cfg(feature = "experimental")]
 pub use rewrite::*;
